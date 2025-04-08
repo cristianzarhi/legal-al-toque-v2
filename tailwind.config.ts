f022089cfd98c0865d29ac0b3ee2bab1b3ede677
Cristian Zarhi <czarhi@gmail.com>
2025-04-07 23:24:33 -0400

    Fix ESLint errors and pass production build

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss"
 
 const config = {
-  darkMode: ["class"],
+  darkMode: "class", // ✅ Corregido
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
